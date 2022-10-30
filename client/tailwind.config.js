2566eacf2fca1a17c6cf9c6d0a858e71852dbf64
abdallaIb <mrlesway5@gmail.com>
2022-10-30 03:17:28 +0000

    feat(client): add config files

---
+++ client/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        // default theme colors
+        main: '#B457F2',
+        main_white: '#F2F2F2',
+        main_side: '#A42EF2',
+        main_side_hover: 'rgba(180 87 242 / 40%)',
+        main_dark: '#291840',
+      },
+    },
+  },
+  plugins: [],
+};
