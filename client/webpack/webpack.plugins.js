3d0a8f62e3fff05d5b6b6466be44e64553a228b8
abdallaIb <mrlesway5@gmail.com>
2022-11-01 22:21:01 +0000

    feat(client): add favicon

--- client/webpack/webpack.plugins.js
+++ client/webpack/webpack.plugins.js
@@ -8,7 +8,7 @@ module.exports = [
   new ForkTsCheckerWebpackPlugin(),
   new HtmlWebpackPlugin({
     template: 'src/index.html',
-    // favicon: 'assets/images/logo.png',
+    favicon: 'src/favicon.ico',
     inject: true,
   }),
   new MiniCssExtractPlugin({
