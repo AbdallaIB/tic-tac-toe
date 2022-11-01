2f7651b5d0cd2cd00e95f05e879ba805d63889f7
abdallaIb <mrlesway5@gmail.com>
2022-11-01 00:24:34 +0000

    feat(client): add modal & countdown timer components

---
+++ client/src/components/countdown-timer/index.tsx
@@ -0,0 +1,47 @@
+import gameContext from '@context/gameContext';
+import { useContext, useEffect, useState } from 'react';
+
+interface ICountdown {
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
+
+const CountDownTimer = ({
+  hours = 0,
+  minutes = 2,
+  seconds = 60,
+  onTimerEnd,
+}: ICountdown & { onTimerEnd: () => void }) => {
+  const { setGameEvents } = useContext(gameContext);
+  const [time, setTime] = useState<ICountdown>({ hours, minutes, seconds });
+
+  const tick = () => {
+    if (time.hours === 0 && time.minutes === 0 && time.seconds === 0) reset();
+    else if (time.seconds === 0) {
+      setTime({ hours: time.hours, minutes: time.minutes - 1, seconds: 59 });
+    } else {
+      setTime({ hours: time.hours, minutes: time.minutes, seconds: time.seconds - 1 });
+    }
+  };
+  const reset = () => {
+    setTime({ hours: time.hours, minutes: time.minutes, seconds: time.seconds });
+    onTimerEnd();
+    setGameEvents((prev: any) => ({ ...prev, isGameOver: true }));
+  };
+
+  useEffect(() => {
+    const timerId = setInterval(() => tick(), 1000);
+    return () => clearInterval(timerId);
+  });
+
+  return (
+    <div className="flex flex-row py-2 px-4 bg-main_dark rounded-xl text-white font-bold text-2xl">
+      <span>{time.minutes}</span>
+      <p className="font-bold text-2xl">:</p>
+      <span>{time.seconds.toString().padStart(2, '0')}</span>
+    </div>
+  );
+};
+
+export default CountDownTimer;
---
+++ client/src/components/modal/index.tsx
@@ -0,0 +1,75 @@
+import './index.css';
+import Modal from 'react-modal';
+import { ReactNode } from 'react';
+import Button from '@components/shared/button';
+
+interface Props {
+  title: string;
+  isOpen: boolean;
+  children?: ReactNode;
+  confirmText: string;
+  styles: { content: { height: string; width: string } };
+  hasFooter: boolean;
+  hasCancelButton: boolean;
+  cancel: () => void;
+  confirm: () => void;
+}
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+
+const Modals = ({
+  title,
+  isOpen,
+  children,
+  cancel,
+  confirm,
+  confirmText,
+  styles,
+  hasFooter = true,
+  hasCancelButton,
+}: Props) => {
+  return (
+    <Modal
+      style={{
+        content: {
+          ...customStyles.content,
+          ...styles.content,
+          overflow: 'hidden',
+          color: 'white',
+          fontSize: '1.5rem',
+          backgroundColor: '#A42EF2',
+        },
+      }}
+      ariaHideApp={false}
+      contentLabel={title}
+      isOpen={isOpen}
+    >
+      <header className="header">
+        <h2 className="font-medium"> {title} </h2>
+        <button className="h-full text-2xl" onClick={() => cancel()}>
+          <i className="bx bx-x"></i>
+        </button>
+      </header>
+      <main className={'flex flex-col items-center justify-center w-full mt-4'} style={{ height: '85%' }}>
+        {children}
+      </main>
+      {hasFooter && (
+        <footer className="footer justify-center">
+          {hasCancelButton && <Button onClick={() => cancel()}>Cancel</Button>}
+          <Button onClick={() => confirm()}>{confirmText}</Button>
+        </footer>
+      )}
+    </Modal>
+  );
+};
+
+export default Modals;
