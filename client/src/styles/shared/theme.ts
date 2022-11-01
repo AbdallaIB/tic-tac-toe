f956c0da3c33f6699810801f4994c23c12a3e874
abdallaIb <mrlesway5@gmail.com>
2022-11-01 00:10:55 +0000

    feat(client): add game context + global styles

---
+++ client/src/styles/shared/theme.ts
@@ -0,0 +1,18 @@
+const theme = {
+  colors: {
+    main: '#B457F2',
+    white: '#F2F2F2',
+    main_side: '#A42EF2',
+    main_dark: '#291840',
+  },
+
+  screens: {
+    xs: '@media(max-width: 576px)',
+    s: '@media(max-width: 576px)',
+    m: '@media(max-width: 768px)',
+    l: '@media(max-width: 992px)',
+    xl: '@media(max-width: 1200px)',
+  },
+};
+
+export default theme;
