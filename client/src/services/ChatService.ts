1e81022855aff45da911943b83ab77017c9864ff
abdallaIb <mrlesway5@gmail.com>
2022-11-15 10:37:14 +0000

    feat(client): add on message received event listener

--- client/src/services/ChatService.ts
+++ client/src/services/ChatService.ts
@@ -2,12 +2,15 @@ import { Message } from '@models/message';
 import { Socket } from 'socket.io-client';
 
 class ChatService {
-  public async sendMessage(socket: Socket, message: string): Promise<Message> {
+  public async sendMessage(socket: Socket, message: string, roomId: string, userId: string): Promise<Message> {
     return new Promise((rs, rj) => {
-      socket.emit('send_message', message);
+      socket.emit('send_message', message, roomId, userId);
       socket.on('message_received', (data: Message) => rs(data));
       socket.on('message_received_error', (error) => rj(error));
     });
+  }
+  public async onMessageReceived(socket: Socket, listener: (message: Message) => void) {
+    socket.on('message_received', (message) => listener(message));
   }
 }
 
